aeb0f358d3e142763167ea35c0cb132690a814b3
Fizuli Ragimov <fizuli@ragimov.ru>
2024-09-29 10:54:37 +0300

    Features/Changed cell_name value. Also make code more readable by making two cycles

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 import tkinter as tk 
 
-
 class Main():
     def __init__(self, root):
         self.init_main()
 
     def move(self, cell_name, cell_figure):
         print(cell_name)
-        
-    def init_main(self):
-        j = 1
-        color = ["white", "black"]
-        for i in range(0, 64):
-            text = str(i + 1)
-            
-    
-            if i % 8 == 0 and j == 0:
-                j = 1
-            elif i % 8 == 0 and j == 1:
-                j = 0
 
-            self.frame = tk.Frame(root)
-            self.frame.grid(row = i // 8, column = i % 8, padx=0, pady=0)
-            cell_name = str(i % 8) + " " + str(i // 8)
-            btn_main_startgame = tk.Button(self.frame, text = color[i%2+j-1], 
-                                           width = 3, height = 3, bd = 0, 
-                                           bg = color[i%2+j-1], fg = color[i%2+j-2],
-                                           activebackground = color[i%2+j-1], 
-                                           activeforeground = color[i%2+j-2],
-                                           command = lambda cell_name = cell_name: self.move(cell_name, "Queen"))
-            btn_main_startgame.pack()
+    def init_main(self):
+        color = ["black", "white"]
+        for i in range(0, 8):
+            for j in range(0, 8):
+                self.frame = tk.Frame(root)
+                self.frame.grid(row = i, column = j, padx = 0, pady = 0)
+                
+                btn_cell = tk.Button(self.frame, text = color[(i + j) % 2], 
+                                            width = 3, height = 3, bd = 0, 
+                                            bg = color[(i + j) % 2], fg = color[(i + j) % 2 - 1],
+                                            activebackground = color[(i + j) % 2], 
+                                            activeforeground = color[(i + j) % 2 - 1],
+                                            command = lambda cell_name = [i, j]: self.move(cell_name, "Queen"))
+                btn_cell.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
