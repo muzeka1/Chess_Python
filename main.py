199237188c55229dc1408d1474f6da0d16dd741f
Fizuli Ragimov <fizuli@ragimov.ru>
2024-09-30 14:55:39 +0300

    Features/New color scheme and fixed some troubles

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
+import tkmacosx as tkx
 import tkinter as tk 
+import platform
 
 class Main():
-    def __init__(self, root):  # исправлено на __init__
+    def __init__(self, root):  
         self.main_figures = ["♖", "♘", "♗", "♔", 
                              "♕", "♗", "♘", "♖"]
-        self.color = ["black", "white"]
+        self.color = ["#FAEBD7", "#6B8E23"]
         self.board = [["" for _ in range(8)] for _ in range(8)]
         self.cells_board = [["" for _ in range(8)] for _ in range(8)]
         self.pressed_btn = None
         self.pressed_btn_text = ""
+        self.pressed_btn_pos = []
         self.isPressed = False
 
         self.init_main(root)
 
     def move(self, cell_name):
         current_btn = self.cells_board[cell_name[0]][cell_name[1]]
+        
         if not self.isPressed and current_btn.cget("text") != "":
-            print("нажали на фигуру ", cell_name)
             if self.pressed_btn:
-                self.pressed_btn.config(bg="SystemButtonFace") 
+                self.pressed_btn.config(bg=self.color[(self.pressed_btn_pos[0] + self.pressed_btn_pos[1]) % 2])
+             
             self.pressed_btn = current_btn
             self.pressed_btn.config(bg="orange")
             self.pressed_btn_text = current_btn.cget("text")
+            self.pressed_btn_pos = [cell_name[0], cell_name[1]]
             self.isPressed = True
 
         elif self.isPressed and current_btn.cget("text") == "":
-            print("делается ход какой-то на клетку ", cell_name)
             current_btn.config(bg=self.color[(cell_name[0] + cell_name[1]) % 2])
-            self.pressed_btn.config(text="")
+            self.pressed_btn.config(text="", bg=self.color[(self.pressed_btn_pos[0] + self.pressed_btn_pos[1]) % 2]) 
             current_btn.config(text=self.pressed_btn_text)
             self.isPressed = False
+
         
 
     def init_main(self, root):
@@ -46,16 +51,20 @@ class Main():
                     text = "♙"
                 else: 
                     text = ""
-                
-                self.btn_cell = tk.Button(self.frame, text=text,
+                if platform.system() == "Darwin":
+                    self.btn_cell = tkx.Button(self.frame)
+                else:
+                    self.btn_cell = tk.Button(self.frame)
+                    
+                self.btn_cell.config(self.frame, text=text,
                                      width=button_size, height=button_size, bd=0, 
                                      bg=self.color[(i + j) % 2], 
-                                     fg=self.color[(i + j + 1) % 2],
+                                     fg="black",#self.color[(i + j + 1) % 2],
                                      activebackground=self.color[(i + j) % 2], 
-                                     activeforeground=self.color[(i + j + 1) % 2],
-                                     font=("Helvetica", "50", "bold"), padx=5, pady=5, 
+                                     activeforeground="black",#self.color[(i + j + 1) % 2],
+                                     font=("Helvetica", "100", "bold"), padx=5, pady=5, 
                                      command=lambda cell_name=[i, j]: self.move(cell_name))
-                
+
                 self.btn_cell.grid(row=i, column=j, sticky='nsew') 
                 self.cells_board[i][j] = self.btn_cell
 
@@ -71,3 +80,4 @@ if __name__ == "__main__":
     root.geometry("700x700")
     root.resizable(False, False)
     root.mainloop()
+
