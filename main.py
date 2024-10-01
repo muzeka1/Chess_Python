8e256add008ab0d00700473e2cc0e09458db9600
Abdulla Khalitov <abdullahalitoff@gmail.com>
2024-10-01 22:03:51 +0300

    pawn turns/ferz turns

--- main.py
+++ main.py
@@ -2,16 +2,104 @@ import tkinter as tk
 import platform
 
 class Movement():
-    def __init__(self, figure):
+    def __init__(self):
+        self.figure = ""
+    
+    def find_possible_move(self, coords, figure, color):
         self.possible_moves = []
         self.figure = figure
-        
-
+        x = coords[0]
+        y = coords[1]
+        print("корды текущей фигуры: ", x, y)
+        if self.figure == "♙":
+            if color == "white":
+                if x == 1 and self.board[x+2][y] == "":
+                    self.possible_moves.append([x+2, y])
+                if x+1 < 8 and self.board[x+1][y] == "":
+                    self.possible_moves.append([x+1, y])
+                if self.board[x+1][y-1] != "" and self.cells_board[x+1][y-1].cget("fg") != color:
+                    self.possible_moves.append([x+1,y-1])
+                if self.board[x+1][y+1] != "" and self.cells_board[x+1][y+1].cget("fg") != color:
+                    self.possible_moves.append([x+1,y+1])
+            if color == "black":
+                if x == 6 and self.board[x-2][y] == "":
+                    self.possible_moves.append([x-2, y])
+                if x-1 >= 0 and self.board[x-1][y] == "":
+                    self.possible_moves.append([x-1, y])
+                if self.board[x-1][y-1] != "" and self.cells_board[x-1][y-1].cget("fg") != color:
+                    self.possible_moves.append([x-1,y-1])
+                if self.board[x-1][y+1] != "" and self.cells_board[x-1][y+1].cget("fg") != color:
+                    self.possible_moves.append([x-1,y+1])
+                    
+        if self.figure == "♕":
+            print(self.board[1][4])
+            
+            print("ферзь думает")
+            for i in range(1, 7):
+                if self.board[x][y-i] != "" or y-i == 0:
+                        self.possible_moves.append([x, y-i])
+                        print("влева низя")
+                        break
+                self.possible_moves.append([x, y-i])
+                print("влево")
+                
+            for i in range(1, 7):
+                if self.board[x][y+i] != "" or y+i == 7:
+                        self.possible_moves.append([x, y+i])
+                        print("вправо низя")
+                        break
+                self.possible_moves.append([x, y+i])
+                print("вправо")
+            
+            for i in range(1, 7):
+                if self.board[x-i][y] != "" or x-i == 0:
+                        self.possible_moves.append([x-i, y])
+                        print("наверх низя")
+                        break
+                self.possible_moves.append([x-i, y])
+                print("наверх")
+            
+            for i in range(1, 7):
+                if self.board[x+i][y] != "" or x+i == 7:
+                        self.possible_moves.append([x+i, y])
+                        print("вниз низя")
+                        break
+                self.possible_moves.append([x+i, y])
+                print("вниз")
+                
+            for i in range(1, 7):
+                if self.board[x-i][y+i] != "" or x-i == 0 or y+i==7:
+                        self.possible_moves.append([x-i, y+i])
+                        break
+                self.possible_moves.append([x-i, y+i])
+                
+            for i in range(1, 7):
+                if self.board[x-i][y-i] != "" or x-i == 0 or y-i==0:
+                        self.possible_moves.append([x-i, y-i])
+                        break
+                self.possible_moves.append([x-i, y-i])
+            
+            for i in range(1, 7):
+                if self.board[x+i][y+i] != "" or x+i == 7 or y+i==7:
+                        self.possible_moves.append([x+i, y+i])
+                        break
+                self.possible_moves.append([x+i, y+i])
+                
+            for i in range(1, 7):
+                if self.board[x+i][y-i] != "" or x+i == 7 or y-i==0:
+                        self.possible_moves.append([x+i, y-i])
+                        break
+                self.possible_moves.append([x+i, y-i])
+                
+                    
 class Main():
     def __init__(self, root):  
         self.main_figures = ["♖", "♘", "♗", "♔", 
                              "♕", "♗", "♘", "♖"]
         self.color = ["#FAEBD7", "#6B8E23"]
+        self.turn = 0
+        self.figure_color = ["white", "black"]
+        self.turn_color = "white"
         self.board = [["" for _ in range(8)] for _ in range(8)]
         self.cells_board = [["" for _ in range(8)] for _ in range(8)]
         self.pressed_btn = None
@@ -24,21 +112,39 @@ class Main():
     def move(self, cell_name):
         current_btn = self.cells_board[cell_name[0]][cell_name[1]]
         
-        if not self.isPressed and current_btn.cget("text") != "":
+        if not self.isPressed and current_btn.cget("text") != "" and current_btn.cget("fg") == self.turn_color:
             if self.pressed_btn:
                 self.pressed_btn.config(bg=self.color[(self.pressed_btn_pos[0] + self.pressed_btn_pos[1]) % 2])
-             
             self.pressed_btn = current_btn
             self.pressed_btn_bg = current_btn.cget("bg")
             self.pressed_btn.config(bg="orange")
             self.pressed_btn_text = current_btn.cget("text")
             self.pressed_btn_pos = [cell_name[0], cell_name[1]]
+            Movement.find_possible_move(self, cell_name, self.pressed_btn_text, self.turn_color)
+            print(self.possible_moves)
             self.isPressed = True
-
-        elif self.isPressed and current_btn.cget("text") == "":
-            self.pressed_btn.config(text="", bg=self.pressed_btn_bg) 
-            current_btn.config(text=self.pressed_btn_text)
+            return
+            
+        if current_btn == self.pressed_btn:
             self.isPressed = False
+            current_btn.config(bg = self.pressed_btn_bg)
+            return
+
+        elif self.isPressed and (current_btn.cget("text") == "" or current_btn.cget("fg") != self.turn_color):
+            if cell_name in self.possible_moves:
+                self.pressed_btn.config(text="", bg=self.pressed_btn_bg) 
+                self.board[self.pressed_btn_pos[0]][self.pressed_btn_pos[1]] = ""
+                self.board[cell_name[0]][cell_name[1]] = self.pressed_btn_text
+                current_btn.config(text=self.pressed_btn_text, fg = self.turn_color)
+                self.turn += 1
+                self.turn_color = self.figure_color[self.turn % 2]
+                self.isPressed = False
+                
+            for i in range(0, 8):
+                print(self.board[i])
+            return
+        
+        
 
 
     def init_main(self, root):
@@ -60,24 +166,24 @@ class Main():
                     self.btn_cell = Button(self.frame, text=text,
                                      width=button_size, height=button_size, bd=0, 
                                      bg=self.color[(i + j) % 2], 
-                                     fg="black",#self.color[(i + j + 1) % 2],
                                      activebackground=self.color[(i + j) % 2], 
-                                     activeforeground="black",#self.color[(i + j + 1) % 2],
                                      font=("Helvetica", "100", "bold"), padx=5, pady=5, 
                                      command=lambda cell_name=[i, j]: self.move(cell_name))
                 else:
                     self.btn_cell = tk.Button(self.frame, text=text,
                                      width=button_size, height=button_size, bd=0, 
-                                     bg=self.color[(i + j) % 2], 
-                                     fg="black",#self.color[(i + j + 1) % 2],
+                                     bg=self.color[(i + j) % 2],
                                      activebackground=self.color[(i + j) % 2], 
-                                     activeforeground="black",#self.color[(i + j + 1) % 2],
                                      font=("Helvetica", "70"), padx=5, pady=5, 
                                      command=lambda cell_name=[i, j]: self.move(cell_name))
                     
-
+                if i in [0,1]:
+                    self.btn_cell.config(fg = "white")
+                if i in [6,7]:
+                    self.btn_cell.config(fg = "black")
                 self.btn_cell.grid(row=i, column=j, sticky='nsew') 
                 self.cells_board[i][j] = self.btn_cell
+                self.board[i][j] = text
 
         for i in range(0, 8):
             self.frame.grid_rowconfigure(i, weight=1)
