a2bdf2c926574c3a41592503e10a32e7b61d291c
muzeka1 <138215289+muzeka1@users.noreply.github.com>
2024-10-01 22:04:47 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import customtkinter as ctk
-
-app = ctk.CTk()  # Используйте CTk
-app.title("Hello World")
-label = ctk.CTkLabel(app, text="Hello, World!")
-label.pack(padx=20, pady=20)
-app.mainloop()
