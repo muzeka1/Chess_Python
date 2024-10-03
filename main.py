f77a1c583e0be868b899ce399e5650ba8ca8040a
Fizuli Ragimov <fizuli@ragimov.ru>
2024-10-03 10:50:53 +0300

    Delete some trash code and made it more readable + added new function to count all moves(in work)

--- main.py
+++ main.py
@@ -1,158 +1,85 @@
 import tkinter as tk 
 import platform
 
+board = [["" for _ in range(8)] for _ in range(8)]
+cells_board = [["" for _ in range(8)] for _ in range(8)]
+
 class Movement():
-    def __init__(self):
-        self.figure = ""
-        self.chah = []
-    
-    def find_possible_move(self, coords, figure, color):
-        self.possible_moves = []
+    def __init__(self, coords, figure, color, possible_moves, flag):
+        self.coords = coords
         self.figure = figure
-        x = coords[0]
-        y = coords[1]
-        print("корды текущей фигуры: ", x, y)
+        self.color = color 
+        self.possible_moves = possible_moves
+        self.flag = flag
+    
+    def all_cycle_directions(self, directions, x, y):
+        for dx, dy in directions:
+            for i in range(1, 8):
+                nx, ny = x + i * dx, y + i * dy
+                if nx < 0 or ny < 0 or nx >= len(board) or ny >= len(board[nx]):
+                    break
+                if board[nx][ny] != "":
+                    if cells_board[nx][ny].cget("fg") != self.color:
+                        self.possible_moves.append([nx, ny])
+                    break
+                self.possible_moves.append([nx, ny])
+
+    def find_possible_move(self):
+        x,y = self.coords[0], self.coords[1]
+
+        #print("корды текущей фигуры: ", x, y)
         if self.figure == "♙":
-            if color == "white":
-                if x == 1 and self.board[x+2][y] == "":
+            if self.color == "white":
+                if x == 1 and board[x+2][y] == "":
                     self.possible_moves.append([x+2, y])
-                if x+1 <= 7 and self.board[x+1][y] == "":
+                if x+1 <= 7 and board[x+1][y] == "":
                     self.possible_moves.append([x+1, y])
-                if x+1 <= 7 and y-1 >= 0 and self.board[x+1][y-1] != "" and self.cells_board[x+1][y-1].cget("fg") != color:
+                if x+1 <= 7 and y-1 >= 0 and board[x+1][y-1] != "" and cells_board[x+1][y-1].cget("fg") != self.color:
                     self.possible_moves.append([x+1,y-1])
-                if x+1 <= 7 and y+1 <= 7 and self.board[x+1][y+1] != "" and self.cells_board[x+1][y+1].cget("fg") != color:
+                if x+1 <= 7 and y+1 <= 7 and board[x+1][y+1] != "" and cells_board[x+1][y+1].cget("fg") != self.color:
                     self.possible_moves.append([x+1,y+1])
-            if color == "black":
-                if x == 6 and self.board[x-2][y] == "":
+            if self.color == "black":
+                if x == 6 and board[x-2][y] == "":
                     self.possible_moves.append([x-2, y])
-                if x-1 >= 0 and self.board[x-1][y] == "":
+                if x-1 >= 0 and board[x-1][y] == "":
                     self.possible_moves.append([x-1, y])
-                if x-1 >= 0 and y-1 >= 0 and self.board[x-1][y-1] != "" and self.cells_board[x-1][y-1].cget("fg") != color:
+                if x-1 >= 0 and y-1 >= 0 and board[x-1][y-1] != "" and cells_board[x-1][y-1].cget("fg") != self.color:
                     self.possible_moves.append([x-1,y-1])
-                if x-1 >= 0 and y+1 <= 7 and self.board[x-1][y+1] != "" and self.cells_board[x-1][y+1].cget("fg") != color:
+                if x-1 >= 0 and y+1 <= 7 and board[x-1][y+1] != "" and cells_board[x-1][y+1].cget("fg") != self.color:
                     self.possible_moves.append([x-1,y+1])
                     
         if self.figure == "♔" :
-            self.possible_moves.append([x-1, y-1])
-            self.possible_moves.append([x+1, y+1])
-            self.possible_moves.append([x, y+1])
-            self.possible_moves.append([x, y-1])
-            self.possible_moves.append([x+1, y])
-            self.possible_moves.append([x-1, y])
-            self.possible_moves.append([x+1, y-1])
-            self.possible_moves.append([x-1, y+1])
+            directions = [(-1, -1), (1, 1), (0, 1), (0, -1), 
+                          (1, 0), (-1, 0), (1, -1), (-1, 1)]
+            for direct in directions:
+                self.possible_moves.append([x + direct[0], y + direct[1]])
                     
         if self.figure == "♕":
-            for i in range(1, 8):
-                if y-i <= 0 or self.board[x][y-i] != "":
-                        self.possible_moves.append([x, y-i])
-                        break
-                self.possible_moves.append([x, y-i])
-                
-            for i in range(1, 8):
-                if y+i >= 7 or self.board[x][y+i] != "":
-                        self.possible_moves.append([x, y+i])
-                        break
-                self.possible_moves.append([x, y+i])
-            
-            for i in range(1, 8):
-                if x-i <= 0 or self.board[x-i][y] != "":
-                        self.possible_moves.append([x-i, y])
-                        break
-                self.possible_moves.append([x-i, y])
-            
-            for i in range(1, 8):
-                if x+i >= 7 or self.board[x+i][y] != "":
-                        self.possible_moves.append([x+i, y])
-                        break
-                self.possible_moves.append([x+i, y])
-                
-            for i in range(1, 8):
-                if x-i == 0 or y+i >= 7 or x == 0 or self.board[x-i][y+i] != "":
-                        self.possible_moves.append([x-i, y+i])
-                        break
-                self.possible_moves.append([x-i, y+i])
-                
-            for i in range(1, 8):
-                if x-i <= 0 or y-i <= 0 or self.board[x-i][y-i] != "":
-                        self.possible_moves.append([x-i, y-i])
-                        break
-                self.possible_moves.append([x-i, y-i])
-            
-            for i in range(1, 8):
-                if x+i >= 7 or y+i >= 7 or self.board[x+i][y+i] != "":
-                        self.possible_moves.append([x+i, y+i])
-                        break
-                self.possible_moves.append([x+i, y+i])
-                
-            for i in range(1, 8):
-                if x+i >= 7 or y-i<=0 or self.board[x+i][y-i] != "":
-                        self.possible_moves.append([x+i, y-i])
-                        break
-                self.possible_moves.append([x+i, y-i])
+            directions = [
+                (0, -1), (0, 1), (-1, 0), (1, 0),
+                (-1, -1), (-1, 1), (1, -1), (1, 1)
+            ]
+            self.all_cycle_directions(directions, x, y)
 
-        
         if self.figure == "♖":
-            for i in range(1, 8):
-                if y-i <= 0 or self.board[x][y-i] != "":
-                        self.possible_moves.append([x, y-i])
-                        break
-                self.possible_moves.append([x, y-i])
-                
-            for i in range(1, 8):
-                if y+i >= 7 or self.board[x][y+i] != "":
-                        self.possible_moves.append([x, y+i])
-                        break
-                self.possible_moves.append([x, y+i])
-            
-            for i in range(1, 8):
-                if x-i <= 0 or self.board[x-i][y] != "":
-                        self.possible_moves.append([x-i, y])
-                        break
-                self.possible_moves.append([x-i, y])
-            
-            for i in range(1, 8):
-                if x+i >= 7 or self.board[x+i][y] != "":
-                        self.possible_moves.append([x+i, y])
-                        break
-                self.possible_moves.append([x+i, y])
+            directions = [
+                (0, -1), (0, 1), (-1, 0), (1, 0),
+            ]
+            self.all_cycle_directions(directions, x, y)
         
         if self.figure == "♗":
-            for i in range(1, 8):
-                if x-i <= 0 or y+i>=7 or self.board[x-i][y+i] != "":
-                        self.possible_moves.append([x-i, y+i])
-                        break
-                self.possible_moves.append([x-i, y+i])
-                
-            for i in range(1, 8):
-                if x-i <= 0 or y-i<=0 or self.board[x-i][y-i] != "":
-                        self.possible_moves.append([x-i, y-i])
-                        break
-                self.possible_moves.append([x-i, y-i])
-            
-            for i in range(1, 8):
-                if x+i >= 7 or y+i>=7 or self.board[x+i][y+i] != "":
-                        self.possible_moves.append([x+i, y+i])
-                        break
-                self.possible_moves.append([x+i, y+i])
-                
-            for i in range(1, 8):
-                if x+i >= 7 or y-i<=0 or self.board[x+i][y-i] != "":
-                        self.possible_moves.append([x+i, y-i])
-                        break
-                self.possible_moves.append([x+i, y-i])
-        
-        if figure == "♘":
-            self.possible_moves.append([x+2, y+1])
-            self.possible_moves.append([x+2, y-1])
-            self.possible_moves.append([x+1, y+2])
-            self.possible_moves.append([x+1, y-2])
-            self.possible_moves.append([x-2, y-1])
-            self.possible_moves.append([x-2, y+1])
-            self.possible_moves.append([x-1, y-2])
-            self.possible_moves.append([x-1, y+2])
-        #for i in range(0, len(possible_moves)):
+            directions = [
+                (-1, -1), (-1, 1), (1, -1), (1, 1)
+            ]
+            self.all_cycle_directions(directions, x, y)
 
+        if self.figure == "♘":
+            directions = [
+                (2, 1), (2, -1), (1, 2), (1, -2), 
+                (-2, -1), (-2, 1), (-1, -2), (-1, 2)
+            ]
+            for direct in directions:
+                self.possible_moves.append([x + direct[0], y + direct[1]])
 
                     
 class Main():
@@ -163,8 +90,7 @@ class Main():
         self.turn = 0
         self.figure_color = ["white", "black"]
         self.turn_color = "white"
-        self.board = [["" for _ in range(8)] for _ in range(8)]
-        self.cells_board = [["" for _ in range(8)] for _ in range(8)]
+        self.possible_moves_main = []
         self.pressed_btn = None
         self.pressed_btn_text = ""
         self.pressed_btn_pos = []
@@ -176,10 +102,19 @@ class Main():
 
         self.init_main(root)
 
+    def near_king_moves_func(self, enemy_color): #В процессе разработки. Функция для подсчета всех ходов всех фигур. Пока не продумал её до конца
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if board[i][j] != "" and board[i][j] != "♔" and cells_board[i][j].cget("fg") == enemy_color:
+                    #print(i, j)
+                    object_class_movement_for_king = Movement([i, j], board[i][j], enemy_color, self.possible_moves_main, True)
+                    #Movement.find_possible_move(self, [i, j], self.board[i][j], enemy_color, True)
+                    
     def move(self, cell_name):
-        current_btn = self.cells_board[cell_name[0]][cell_name[1]]
+        current_btn = cells_board[cell_name[0]][cell_name[1]]
         
         if not self.isPressed and current_btn.cget("text") != "" and current_btn.cget("fg") == self.turn_color:
+            print("ВЫБРАЛИ ФИГУРУ")
             if self.pressed_btn:
                 self.pressed_btn.config(bg=self.color[(self.pressed_btn_pos[0] + self.pressed_btn_pos[1]) % 2])
             self.pressed_btn = current_btn
@@ -188,8 +123,10 @@ class Main():
             self.pressed_btn_text = current_btn.cget("text")
             self.pressed_btn_pos = [cell_name[0], cell_name[1]]
             self.pressed_btn_fg = current_btn.cget("fg")
-            Movement.find_possible_move(self, cell_name, self.pressed_btn_text, self.turn_color)
-            print(self.possible_moves)
+
+            object_class_movement = Movement(cell_name, self.pressed_btn_text, self.turn_color, self.possible_moves_main, False)
+            object_class_movement.find_possible_move()
+            
             self.isPressed = True
             return
             
@@ -201,35 +138,43 @@ class Main():
             self.pressed_btn.config(bg="orange")
             self.pressed_btn_text = current_btn.cget("text")
             self.pressed_btn_pos = [cell_name[0], cell_name[1]]
-            Movement.find_possible_move(self, cell_name, self.pressed_btn_text, self.turn_color)
-            print(self.possible_moves)
+
+            object_class_movement = Movement(cell_name, self.pressed_btn_text, self.turn_color, self.possible_moves_main, False)
+            object_class_movement.find_possible_move()
+         
             self.isPressed = True
             return
 
         if self.isPressed and (current_btn.cget("text") == "" or current_btn.cget("fg") != self.turn_color):
             print("ПОСТАВИЛИ ФИГУРУ", self.turn_color)
-            if cell_name in self.possible_moves:
+            print(self.possible_moves_main)
+            if cell_name in self.possible_moves_main:
                 self.pressed_btn.config(text="", bg=self.pressed_btn_bg) 
-                self.board[self.pressed_btn_pos[0]][self.pressed_btn_pos[1]] = ""
-                self.board[cell_name[0]][cell_name[1]] = self.pressed_btn_text
+                board[self.pressed_btn_pos[0]][self.pressed_btn_pos[1]] = ""
+                board[cell_name[0]][cell_name[1]] = self.pressed_btn_text
                 current_btn.config(text=self.pressed_btn_text, fg = self.turn_color)
+
+                if current_btn.cget("text") == "♔":
+                    if self.turn_color == "black":
+                        self.black_king = cell_name
+                        
+                    if self.turn_color == "white":
+                        self.white_king = cell_name
+
                 self.turn += 1
                 self.turn_color = self.figure_color[self.turn % 2]
                 self.isPressed = False
+                self.possible_moves_main = []
+                self.near_king_moves_func(self.turn_color) #Вызываем функцию поиска всех ходов всех фигур.
 
-            print(self.turn_color)
-                
-            #for i in range(0, 8):
-             #   print(self.board[i])
+                for i in range(0, len(board)):
+                    print(board[i])
             return
-        
-        
 
 
     def init_main(self, root):
         self.frame = tk.LabelFrame(root)
         self.frame.pack(expand=True)
-
         button_size = 80  
 
         for i in range(0, 8):
@@ -261,8 +206,8 @@ class Main():
                 if i in [6,7]:
                     self.btn_cell.config(fg = "black")
                 self.btn_cell.grid(row=i, column=j, sticky='nsew') 
-                self.cells_board[i][j] = self.btn_cell
-                self.board[i][j] = text
+                cells_board[i][j] = self.btn_cell
+                board[i][j] = text
 
         for i in range(0, 8):
             self.frame.grid_rowconfigure(i, weight=1)
