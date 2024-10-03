fb9a37bcd4f26f830d141c1d767baf2e2586ada8
Fizuli Ragimov <fizuli@ragimov.ru>
2024-10-03 13:42:08 +0300

    Features/Delete near_king_possible_moves func and changed names and colors

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Movement():
         self.color = color 
         self.possible_moves = possible_moves
         self.flag = flag
-    
+
     def all_cycle_directions(self, directions, x, y):
         for dx, dy in directions:
             for i in range(1, 8):
@@ -29,52 +29,52 @@ class Movement():
         x,y = self.coords[0], self.coords[1]
 
         #print("корды текущей фигуры: ", x, y)
-        if self.figure == "♙":
+        if self.figure == "♟":
             if self.color == "white":
                 if x == 1 and board[x+2][y] == "":
                     self.possible_moves.append([x+2, y])
                 if x+1 <= 7 and board[x+1][y] == "":
                     self.possible_moves.append([x+1, y])
-                if x+1 <= 7 and y-1 >= 0 and board[x+1][y-1] != "" and cells_board[x+1][y-1].cget("fg") != self.color:
+                if x+1 <= 7 and y-1 >= 0 and ((board[x+1][y-1] != "" and cells_board[x+1][y-1].cget("fg") != self.color) or self.flag == True):
                     self.possible_moves.append([x+1,y-1])
-                if x+1 <= 7 and y+1 <= 7 and board[x+1][y+1] != "" and cells_board[x+1][y+1].cget("fg") != self.color:
+                if x+1 <= 7 and y+1 <= 7 and ((board[x+1][y+1] != "" and cells_board[x+1][y+1].cget("fg") != self.color) or self.flag == True):
                     self.possible_moves.append([x+1,y+1])
             if self.color == "black":
                 if x == 6 and board[x-2][y] == "":
                     self.possible_moves.append([x-2, y])
                 if x-1 >= 0 and board[x-1][y] == "":
                     self.possible_moves.append([x-1, y])
-                if x-1 >= 0 and y-1 >= 0 and board[x-1][y-1] != "" and cells_board[x-1][y-1].cget("fg") != self.color:
+                if x-1 >= 0 and y-1 >= 0 and ((board[x-1][y-1] != "" and cells_board[x-1][y-1].cget("fg") != self.color) or self.flag == True):
                     self.possible_moves.append([x-1,y-1])
-                if x-1 >= 0 and y+1 <= 7 and board[x-1][y+1] != "" and cells_board[x-1][y+1].cget("fg") != self.color:
+                if x-1 >= 0 and y+1 <= 7 and ((board[x-1][y+1] != "" and cells_board[x-1][y+1].cget("fg") != self.color) or self.flag == True):
                     self.possible_moves.append([x-1,y+1])
                     
-        if self.figure == "♔" :
+        if self.figure == "♚" :
             directions = [(-1, -1), (1, 1), (0, 1), (0, -1), 
                           (1, 0), (-1, 0), (1, -1), (-1, 1)]
             for direct in directions:
                 self.possible_moves.append([x + direct[0], y + direct[1]])
                     
-        if self.figure == "♕":
+        if self.figure == "♛":
             directions = [
                 (0, -1), (0, 1), (-1, 0), (1, 0),
                 (-1, -1), (-1, 1), (1, -1), (1, 1)
             ]
             self.all_cycle_directions(directions, x, y)
 
-        if self.figure == "♖":
+        if self.figure == "♜":
             directions = [
                 (0, -1), (0, 1), (-1, 0), (1, 0),
             ]
             self.all_cycle_directions(directions, x, y)
         
-        if self.figure == "♗":
+        if self.figure == "♝":
             directions = [
                 (-1, -1), (-1, 1), (1, -1), (1, 1)
             ]
             self.all_cycle_directions(directions, x, y)
 
-        if self.figure == "♘":
+        if self.figure == "♞":
             directions = [
                 (2, 1), (2, -1), (1, 2), (1, -2), 
                 (-2, -1), (-2, 1), (-1, -2), (-1, 2)
@@ -85,13 +85,14 @@ class Movement():
                     
 class Main():
     def __init__(self, root):  
-        self.main_figures = ["♖", "♘", "♗", "♔", 
-                             "♕", "♗", "♘", "♖"]
-        self.color = ["#FFEBCD", "#6B8E23"]#FAEBD7
+        self.main_figures = ["♜", "♞", "♝", "♚", 
+                             "♛", "♝", "♞", "♜"]
+        self.color = ["#c8c8c8", "#6B8E23"]#FAEBD7
         self.turn = 0
         self.figure_color = ["white", "black"]
         self.turn_color = "white"
-        self.possible_moves_main = []
+        self.possible_moves_figure = []
+        
         self.pressed_btn = None
         self.pressed_btn_text = ""
         self.pressed_btn_pos = []
@@ -102,17 +103,9 @@ class Main():
         self.white_king = [0, 3]
 
         self.init_main(root)
-
-    def near_king_moves_func(self, enemy_color): #В процессе разработки. Функция для подсчета всех ходов всех фигур. Пока не продумал её до конца
-        for i in range(0, 8):
-            for j in range(0, 8):
-                if board[i][j] != "" and board[i][j] != "♔" and cells_board[i][j].cget("fg") == enemy_color:
-                    #print(i, j)
-                    object_class_movement_for_king = Movement([i, j], board[i][j], enemy_color, self.possible_moves_main, True)
-                    #Movement.find_possible_move(self, [i, j], self.board[i][j], enemy_color, True)
                     
-    def move(self, cell_name):
-        current_btn = cells_board[cell_name[0]][cell_name[1]]
+    def move(self, cell_cords):
+        current_btn = cells_board[cell_cords[0]][cell_cords[1]]
         
         if not self.isPressed and current_btn.cget("text") != "" and current_btn.cget("fg") == self.turn_color:
             print("ВЫБРАЛИ ФИГУРУ")
@@ -122,15 +115,15 @@ class Main():
             self.pressed_btn_bg = current_btn.cget("bg")
             self.pressed_btn.config(bg="orange")
             self.pressed_btn_text = current_btn.cget("text")
-            self.pressed_btn_pos = [cell_name[0], cell_name[1]]
+            self.pressed_btn_pos = [cell_cords[0], cell_cords[1]]
             self.pressed_btn_fg = current_btn.cget("fg")
 
-            object_class_movement = Movement(cell_name, self.pressed_btn_text, self.turn_color, self.possible_moves_main, False)
+            object_class_movement = Movement(cell_cords, self.pressed_btn_text, self.turn_color, self.possible_moves_figure, False)
             object_class_movement.find_possible_move()
             
             self.isPressed = True
             return
-            
+
         if current_btn.cget("fg") == self.pressed_btn_fg and current_btn.cget("fg") == self.turn_color and current_btn.cget("text") != "":
             print("ПОМЕНЯЛИ ФИГУРУ")
             self.pressed_btn.config(bg = self.pressed_btn_bg)
@@ -138,38 +131,37 @@ class Main():
             self.pressed_btn_bg = current_btn.cget("bg")
             self.pressed_btn.config(bg="orange")
             self.pressed_btn_text = current_btn.cget("text")
-            self.pressed_btn_pos = [cell_name[0], cell_name[1]]
+            self.pressed_btn_pos = [cell_cords[0], cell_cords[1]]
 
-            object_class_movement = Movement(cell_name, self.pressed_btn_text, self.turn_color, self.possible_moves_main, False)
+            object_class_movement = Movement(cell_cords, self.pressed_btn_text, self.turn_color, self.possible_moves_figure, False)
             object_class_movement.find_possible_move()
          
             self.isPressed = True
             return
 
         if self.isPressed and (current_btn.cget("text") == "" or current_btn.cget("fg") != self.turn_color):
-            print("ПОСТАВИЛИ ФИГУРУ", self.turn_color)
-            print(self.possible_moves_main)
-            if cell_name in self.possible_moves_main:
+            if cell_cords in self.possible_moves_figure:
+                print("ПОСТАВИЛИ ФИГУРУ", self.turn_color)
+                print(self.possible_moves_figure)
                 self.pressed_btn.config(text="", bg=self.pressed_btn_bg) 
                 board[self.pressed_btn_pos[0]][self.pressed_btn_pos[1]] = ""
-                board[cell_name[0]][cell_name[1]] = self.pressed_btn_text
+                board[cell_cords[0]][cell_cords[1]] = self.pressed_btn_text
                 current_btn.config(text=self.pressed_btn_text, fg = self.turn_color)
 
-                if current_btn.cget("text") == "♔":
+                if current_btn.cget("text") == "♚":
                     if self.turn_color == "black":
-                        self.black_king = cell_name
+                        self.black_king = cell_cords
                         
                     if self.turn_color == "white":
-                        self.white_king = cell_name
+                        self.white_king = cell_cords
 
                 self.turn += 1
                 self.turn_color = self.figure_color[self.turn % 2]
                 self.isPressed = False
-                self.possible_moves_main = []
-                self.near_king_moves_func(self.turn_color) #Вызываем функцию поиска всех ходов всех фигур.
+                self.possible_moves_figure = []
 
-                for i in range(0, len(board)):
-                    print(board[i])
+                #for i in range(0, len(board)):
+                #    print(board[i])
             return
 
 
@@ -183,24 +175,24 @@ class Main():
                 if i == 0 or i == 7:
                     text = self.main_figures[j]
                 elif i == 1 or i == 6:
-                    text = "♙"
+                    text = "♟"
                 else: 
                     text = ""
                 if platform.system() == "Darwin":
                     from tkmacosx import Button
                     self.btn_cell = Button(self.frame, text=text,
-                                     width=button_size, height=button_size, bd=0, 
+                                     width=button_size, height=button_size, bd=0,
                                      bg=self.color[(i + j) % 2], 
                                      activebackground=self.color[(i + j) % 2], 
                                      font=("Helvetica", "100", "bold"), padx=5, pady=5, 
-                                     command=lambda cell_name=[i, j]: self.move(cell_name))
+                                     command=lambda cell_cords=[i, j]: self.move(cell_cords))
                 else:
                     self.btn_cell = tk.Button(self.frame, text=text,
                                      width=button_size, height=button_size, bd=0, 
                                      bg=self.color[(i + j) % 2],
                                      activebackground=self.color[(i + j) % 2], 
                                      font=("Helvetica", "70"), padx=5, pady=5, 
-                                     command=lambda cell_name=[i, j]: self.move(cell_name))
+                                     command=lambda cell_cords=[i, j]: self.move(cell_cords))
                     
                 if i in [0,1]:
                     self.btn_cell.config(fg = "white")
@@ -224,3 +216,5 @@ if __name__ == "__main__":
     photo = ImageTk.PhotoImage(ico)
     root.wm_iconphoto(False, photo)
     root.mainloop()
+
+    
